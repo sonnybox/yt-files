8b553ce0e1e3ab080c11bf886cf8e649bd8ea528
Sonny Box <sonny@sonnybox.com>
2026-01-31 22:22:35 -0800

    add cuda tools only

--- COMFY/runpod_scripts/generic_sage.sh
+++ COMFY/runpod_scripts/generic_sage.sh
@@ -3,9 +3,11 @@ echo 'cd /root' >> /etc/bash.bashrc
 echo 'source venv/bin/activate' >> /etc/bash.bashrc
 cd /root
 
-wget https://archive.archlinux.org/packages/c/cuda/cuda-12.8.1-3-x86_64.pkg.tar.zst -O /root/cuda.tar.zst
-wget https://archive.archlinux.org/packages/g/gcc14/gcc14-14.3.1%2Br416%2Bg44d5743651c4-2-x86_64.pkg.tar.zst -O /root/gcc.tar.zst
-wget https://archive.archlinux.org/packages/g/gcc14-libs/gcc14-libs-14.3.1%2Br416%2Bg44d5743651c4-2-x86_64.pkg.tar.zst -O /root/gcc-libs.tar.zst
+MIRROR=https://archive.archlinux.org
+
+wget $MIRROR/packages/c/cuda-tools/cuda-tools-12.8.1-3-x86_64.pkg.tar.zst -O /root/cuda.tar.zst
+wget $MIRROR/packages/g/gcc14/gcc14-14.3.1%2Br416%2Bg44d5743651c4-2-x86_64.pkg.tar.zst -O /root/gcc.tar.zst
+wget $MIRROR/packages/g/gcc14-libs/gcc14-libs-14.3.1%2Br416%2Bg44d5743651c4-2-x86_64.pkg.tar.zst -O /root/gcc-libs.tar.zst
 
 pacman -U --noconfirm /root/gcc-libs.tar.zst /root/gcc.tar.zst
 pacman -U --noconfirm /root/cuda.tar.zst
