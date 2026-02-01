6bf66123a53a198b3a2335f08949122e9c9198eb
Sonny Box <sonny@sonnybox.com>
2026-01-31 23:09:50 -0800

    stable script

--- COMFY/runpod_scripts/generic_sage.sh
+++ COMFY/runpod_scripts/generic_sage.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
+
 echo 'cd /root' >> /etc/bash.bashrc
 echo 'source venv/bin/activate' >> /etc/bash.bashrc
+
 cd /root
 
-MIRROR=https://archive.archlinux.org
-
-wget $MIRROR/packages/c/cuda-tools/cuda-tools-12.8.1-3-x86_64.pkg.tar.zst -O /root/cuda.tar.zst
-wget $MIRROR/packages/g/gcc14/gcc14-14.3.1%2Br416%2Bg44d5743651c4-2-x86_64.pkg.tar.zst -O /root/gcc.tar.zst
-wget $MIRROR/packages/g/gcc14-libs/gcc14-libs-14.3.1%2Br416%2Bg44d5743651c4-2-x86_64.pkg.tar.zst -O /root/gcc-libs.tar.zst
-
-pacman -U --noconfirm /root/gcc-libs.tar.zst /root/gcc.tar.zst
-pacman -U --noconfirm /root/cuda.tar.zst
-
-export PATH="$CUDA_HOME/bin:$PATH"
-
 git clone https://github.com/Comfy-Org/ComfyUI
-git clone https://github.com/thu-ml/SageAttention
 git clone https://github.com/Comfy-Org/ComfyUI-Manager ComfyUI/custom_nodes/ComfyUI-Manager
 
-uv venv venv --python $PYTHON_VERSION
+cd ComfyUI
+git fetch --tags
+git checkout $(git tag --sort=-v:refname | head -n 1)
+cd ..
+
+uv venv venv --python 3.12
 source venv/bin/activate
 
 uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
+uv pip install comfy-cli
 uv pip install -r ComfyUI/requirements.txt
 uv pip install -r ComfyUI/custom_nodes/ComfyUI-Manager/requirements.txt
 
-cd SageAttention
-git checkout v2.2.0
-uv pip install setuptools ninja
-uv pip install . --no-build-isolation
-cd ..
+# thanks kijai
+uv pip install https://huggingface.co/Kijai/PrecompiledWheels/resolve/main/sageattention-2.2.0-cp312-cp312-linux_x86_64.whl
 
-python ComfyUI/main.py --listen
-sleep infinity
+python main.py \
+	--listen \
+	--preview-method auto \
+	--max-upload-size 9999999 \
+	--extra-model-paths-config ~/comfy-user/models.yaml
