68f38770039a538b38f0553774f015145075f8a2
Sonny Box <sonny@sonnybox.com>
2026-01-31 21:36:03 -0800

    downgrade gcc

--- COMFY/runpod_scripts/generic_sage.sh
+++ COMFY/runpod_scripts/generic_sage.sh
@@ -4,6 +4,8 @@ echo 'source venv/bin/activate' >> /etc/bash.bashrc
 cd /root
 
 wget https://archive.archlinux.org/packages/c/cuda/cuda-12.8.1-3-x86_64.pkg.tar.zst -O /root/cuda.tar.zst
+wget https://archive.archlinux.org/packages/g/gcc/gcc-14.2.1%2Br753%2Bg1cd744a6828f-1-x86_64.pkg.tar.zst -O /root/gcc.tar.zst
+pacman -U /root/gcc.tar.zst --noconfirm
 pacman -U /root/cuda.tar.zst --noconfirm
 
 export PATH="$CUDA_HOME/bin:$PATH"
