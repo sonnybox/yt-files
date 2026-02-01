82c1318b6949c5a9171b4ae08a6eda5fea717fbe
Sonny Box <sonny@sonnybox.com>
2026-01-31 23:26:46 -0800

    stable script!?

--- COMFY/runpod_scripts/generic_sage.sh
+++ COMFY/runpod_scripts/generic_sage.sh
@@ -3,34 +3,28 @@
 echo 'cd /root' >> /etc/bash.bashrc
 echo 'source venv/bin/activate' >> /etc/bash.bashrc
 
-# /root
 cd /root
 
 git clone https://github.com/Comfy-Org/ComfyUI
 git clone https://github.com/Comfy-Org/ComfyUI-Manager ComfyUI/custom_nodes/ComfyUI-Manager
 
-# /root/ComfyUI
 cd ComfyUI
 
 git fetch --tags
 git checkout $(git tag --sort=-v:refname | head -n 1)
-
-# /root
-cd ..
 
 uv venv venv --python 3.12
 source venv/bin/activate
 
 uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 uv pip install comfy-cli
-uv pip install -r /root/ComfyUI/requirements.txt
-uv pip install -r /root/ComfyUI/custom_nodes/ComfyUI-Manager/requirements.txt
+uv pip install -r requirements.txt
+uv pip install -r custom_nodes/ComfyUI-Manager/requirements.txt
 
 # thanks kijai
 uv pip install https://huggingface.co/Kijai/PrecompiledWheels/resolve/main/sageattention-2.2.0-cp312-cp312-linux_x86_64.whl
 
-python /root/ComfyUI/main.py \
+python main.py \
 	--listen \
 	--preview-method auto \
 	--max-upload-size 9999999 \
-	--extra-model-paths-config ~/comfy-user/models.yaml
